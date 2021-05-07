53457590f566ebf20649a61792e542082f4239c2
geof <>
2021-05-07 12:43:35 +0300

    added serverside rendering for movie results, added a thumbnail component

---
+++ utils/requests.js
@@ -0,0 +1,48 @@
+const API_KEY = process.env.API_KEY;
+
+export default {
+  fetchTrending: {
+    title: "Trending",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  },
+  fetchComedyMovies: {
+    title: "Comedy",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  },
+  fetchHorrorMovies: {
+    title: "Trending",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  fetchRomanceMovies: {
+    title: "Romance",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  },
+  fetchMystery: {
+    title: "Trending",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  },
+  fetchSciFi: {
+    title: "SciFi",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  },
+  fetchWestern: {
+    title: "Western",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  },
+  fetchAnimation: {
+    title: "Animation",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  },
+  fetchTV: {
+    title: "TV Movie",
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  }
+};
